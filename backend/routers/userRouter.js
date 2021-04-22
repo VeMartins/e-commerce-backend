f66eb302d60d0743ed35d86aed36e1022ceb4971
VeMartins <66840333+VeMartins@users.noreply.github.com>
2021-04-22 19:08:10 +0200

    created new route in userRouter to update userProfile

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -4,7 +4,7 @@ import bcrypt from "bcryptjs";
 
 import User from "../models/userModel.js";
 import data from "../sampleUsers.js";
-import { generateToken } from "../utils.js";
+import { generateToken, isAuth } from "../utils.js";
 
 const userRouter = express.Router();
 
@@ -58,6 +58,42 @@ userRouter.post(
       isAdmin: createdUser.isAdmin,
       token: generateToken(createdUser),
     });
+  })
+);
+userRouter.get(
+  "/:id",
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      res.send(user);
+    } else {
+      res.status(404).send({ message: "User Not Found" });
+    }
+  })
+);
+
+// updating user profile
+userRouter.put(
+  "/userprofile",
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+      user.name = req.body.name || user.name; // || user.name --> in case user leaves the field empty
+      user.email = req.body.email || user.email;
+      if (req.body.password) {
+        user.password = bcrypt.hashSync(req.body.password, 8);
+      }
+
+      const updatedUser = await user.save();
+      res.send({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+        token: generateToken(updatedUser),
+      });
+    }
   })
 );
 export default userRouter;
